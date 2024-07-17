79864de19c8624d8f1121e66814fe3cf7ce73a6a
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-07-17 14:29:39 +0530

    update: day2 operator

--- Day2_Operators.js
+++ Day2_Operators.js
@@ -1,3 +1,4 @@
+// ****************************************** Task 1-4 *************************************************
 // WAP to find sub,diifrence,product and division of two numbers and  log the output in console.
 
 let a= 4;
@@ -8,6 +9,7 @@ console.log(`${a} - ${b} = ${a-b}`);
 console.log(`${a} * ${b} = ${a*b}`);
 console.log(`${a} / ${b} = ${a/b}`);
 
+// ****************************************** Task 5 *************************************************
 // WAP to find the reminder when a num is divided by other num.
 
 let n1=34;
@@ -16,6 +18,7 @@ let n2=26;
 console.log(`${n1} % ${n2} = ${n1%n2}`);
 
 //  +=, -= operator to add and substract a num from other num.
+// ****************************************** Task 6-7 *************************************************
 
 a+=9;
 b-=3;
@@ -26,59 +29,66 @@ console.log(`a and b after operation a:${a} b:${b}`);
 //  use '<', '>', '<=' ,'=>', '==', '===, 
 // Coercion(implicit conversion of one type of data type to another) as 9 is implicitly converted string to num.
 
+// ****************************************** Task 8 *************************************************
 if(8<'9') // true
-    console.log(true);
+console.log(true);
 else 
-    console.log(false);
+console.log(false);
 
 
 if(8>'9') //false
-    console.log(true);
+console.log(true);
 else 
-    console.log(false);
+console.log(false);
 
 
+// ****************************************** Task 9 *************************************************
 if(8<='9') //true
-    console.log(true);
+console.log(true);
 else 
-    console.log(false);
+console.log(false);
 
 
 if(8>='9') //false
-    console.log(true);
+console.log(true);
 else 
-    console.log(false);
+console.log(false);
 
+// ****************************************** Task 10 *************************************************
 if(8=='8') //true 
-    console.log(true);
+console.log(true);
 else 
-    console.log(false);
+console.log(false);
 
 if(8==='8') //false
-    console.log(true);
+console.log(true);
 else 
-    console.log(false);
+console.log(false);
 
 
 
-    if((a>b) &&(n2<n1) ){ // true
-        console.log('&&-both condition is true ');
-    }
-    else
-    console.log('&&-both or any one of the conditions is false ');
+// ****************************************** Task 11 *************************************************
+if((a>b) &&(n2<n1) ){ // true
+    console.log('&&-both condition is true ');
+}
+else
+console.log('&&-both or any one of the conditions is false ');
 
-    if((a>b)||(n2<n1) ){ //true
-        console.log('|| -both or any one of condition is true ');
-    }
-    else
-    console.log('||-both conditions are false ');
+// ****************************************** Task 12 *************************************************
+if((a>b)||(n2<n1) ){ //true
+    console.log('|| -both or any one of condition is true ');
+}
+else
+console.log('||-both conditions are false ');
 
-    if(!n1 ){ //false
-        console.log(`given n1 value is false so result is !false ${!n1}`);
-    }
-    else
-    console.log(`given n1 value is true so result is !true ${!n1}`);
+// ****************************************** Task 13 *************************************************
+if(!n1 ){ //false
+    console.log(`given n1 value is false so result is !false ${!n1}`);
+}
+else
+console.log(`given n1 value is true so result is !true ${!n1}`);
 
 
-    let n=-4;
-    (n >0)?console.log('positive'):console.log('negative'); //negative
+// ****************************************** Task 14 *************************************************
+let n=-4;
+(n >0)?console.log('positive'):console.log('negative'); //negative
